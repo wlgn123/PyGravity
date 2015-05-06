df0072874d76bd756de5ef89b31cfb49ae843798
Russ <russloewe@gmail.com>
2015-05-06 12:46:36 -0700

    added tests for big numbers

--- test.py
+++ test.py
@@ -46,6 +46,26 @@ class Local_files_tests(unittest.TestCase):
         self.failUnless(x == 0 )
         self.failUnless(y == 0 )
         self.failUnless(z == 0 )
+        
+    def test_Vector_Addition_bignumber(self):
+        BigA = Vector(1.3*10**12, 4.0*10**12, 6.3*10**13)
+        BigB = Vector(1.1*10**12, 4.0*10**12, 6.1*10**13)
+        C = BigA.add(BigB)
+        x = C.x
+        y = C.y
+        z = C.z
+        self.failUnless(x == 2.4*10**12 )
+        self.failUnless(y == 8.0*10**12 )
+        self.failUnless(z == 1.24*10**14 )
+        
+    def test_Vector_Addition_biger_number(self):
+        BigA = Vector(1.3*10**24, 4.0*10**24, 6.3*10**13)
+        BigB = Vector(1.1*10**24, 4.0*10**24, 6.1*10**13)
+        Answer = Vector(2.4*10**24, 8.0*10**24, 1.24*10**14)
+        C = BigA.add(BigB)
+        self.failUnless(C.y == Answer.y )
+        self.failUnless(C.y == Answer.y)
+        self.failUnless(C.z == Answer.z)
 
 
 def main():
