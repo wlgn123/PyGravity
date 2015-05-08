bcb61cda36e48da6bf0b3e0ffe2f95611e0c429b
Russ <russloewe@gmail.com>
2015-05-07 20:19:10 -0700

    added note to remined myself of something

--- libs/Physics.py
+++ libs/Physics.py
@@ -6,3 +6,6 @@ class Physics(object):
     @staticmethod
     def Fg(A, B):
         pass 
+#For summing the force of gravity on the particle, calcuate
+# the force for each particle, add to list the use fancy python reduce
+# or  something to sum the list using the Vector.add() function
