7ea6cc92829b7c7b5fc5b733d85f30498662534e
Russ <russloewe@gmail.com>
2015-05-19 20:17:52 -0700

    testing out some scenarioes

--- PyGravity.py
+++ PyGravity.py
@@ -1,14 +1,28 @@
-from libs import Vector
+from libs import Vector, Particle, Physics
 
-a = Vector(1,2,1)
-b = Vector(1,1,0)
-print " Partical A at ", a.show
-print " Partical B at ", b.show
+A = Particle(Vector(0,10.00,0), Vector(0,0,0), 1)
+B = Particle(Vector(2.00,0,0), Vector(0,0,0), 100000000000)
+C = Particle(Vector(-2.00,0,0), Vector(0,0,0), 100000000000)
+D = Particle(Vector(4.00,0,0), Vector(0,0,0), 100000000000)
+
+base = Physics()
+
+base.add_obj(A)
+base.add_obj(B)
+base.add_obj(C)
+#base.add_obj(D)
+
+for item in base.objects:
+    print item.P.show
+
+f = base.sum_Fg_one_particle(base.objects[0])
+for i in range(500):
+    base.apply_gravitational_acceleration(base.objects[0])
+    base.objects[0].move()
+    print 'A: ', base.objects[0].P.y
+    #print "force: ", f.show
+    #print "volecity: ", base.objects[0].V.show
 
 
-for i in range(50000):
-    str_a = a.show
-    str_b = b.show
-    a = a.add(b)    
-    print str_a, '+', str_b, '=', a.show
-
+for item in base.objects:
+    print item.P.show
