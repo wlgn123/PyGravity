f79e730ac320bab339e12fc1d611eb53cb038030
Russ <russloewe@gmail.com>
2015-05-23 19:17:12 -0700

    removed vector class

--- libs/Particle.py
+++ libs/Particle.py
@@ -1,4 +1,4 @@
-from Vector import Vector
+import numpy as np
 class Particle(object):
     def __init__(self, P, V, m):
         self.P = P        #particles position vector
@@ -6,10 +6,10 @@ class Particle(object):
         self.m = m        #particles mass
         
     def move(self):
-        new_pos = Vector.add(self.P, self.V)
+        new_pos = np.add(self.P, self.V)
         self.P = new_pos
         
     def accelerate(self, A):
-        new = Vector.add(self.V, A)
+        new = np.add(self.V, A)
         self.V = new
         
--- libs/Physics.py
+++ libs/Physics.py
@@ -1,4 +1,5 @@
-from Vector import Vector
+import numpy as np
+from numpy import linalg as LA
 from Particle import Particle
 class Physics(object):
     def __init__(self):
@@ -12,11 +13,11 @@ class Physics(object):
         G = 6.67384 * 10**(-11)
         ma = A.m
         mb = B.m
-        r = Vector.sub(A.P, B.P)  #vector between two particles
-        r_hat = Vector.unit(r)    #unit vector of r
-        r_squared = Vector.magnitude(r) ** 2  # dist between A, B squared
+        r = np.subtract(A.P, B.P)  #vector between two particles
+        r_hat = np.multiply((1/LA.norm(r)), r)    #unit vector of r
+        r_squared = LA.norm(r) ** 2  # dist between A, B squared
         f_mag = (G*ma*mb)/r_squared
-        f_vec = Vector.times_scalar(f_mag, r_hat)
+        f_vec = np.multiply(f_mag, r_hat)
         return f_vec
 
     def sum_Fg_one_particle(self, A):
@@ -24,13 +25,13 @@ class Physics(object):
         for particle in self.objects:
             if particle != A:
                 force_list.append(self.Fg(particle, A))
-        f = lambda a,b: Vector.add(a,b)
+        f = lambda a,b: np.add(a,b)
         total_force = reduce(f, force_list)
         return total_force
 
     def apply_gravitational_acceleration(self, A):
         total_Fg_A = self.sum_Fg_one_particle(A)
-        acceleration = Vector.times_scalar((1/A.m), total_Fg_A)
+        acceleration = np.multiply((1/A.m), total_Fg_A)
         A.accelerate(acceleration)
 
 #At delta time to accelerate and move methods in Particle class
