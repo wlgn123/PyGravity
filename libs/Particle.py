021112c29d54032b1ffe7c303cc42bbb0d49ff17
Russ <russloewe@gmail.com>
2015-06-01 16:11:36 -0700

    changing to Vector, added __str__

--- libs/Particle.py
+++ libs/Particle.py
@@ -1,15 +1,19 @@
-import numpy as np
+from Vector import Vector
 class Particle(object):
     def __init__(self, P, V, m):
         self.P = P        #particles position vector
         self.V = V        #Particles velocity vector
         self.m = m        #particles mass
-        
+
     def move(self):
-        new_pos = np.add(self.P, self.V)
+        new_pos = self.P + self.V
         self.P = new_pos
-        
+
     def accelerate(self, A):
-        new = np.add(self.V, A)
+        new = self.V + A
         self.V = new
-        
+
+    def __str__(self):
+        particle_str = 'Position: ' + str(self.P) + '\nVelocity: ' + str(self.V) + '\nMass: ' + str(self.m)
+        return particle_str
+
