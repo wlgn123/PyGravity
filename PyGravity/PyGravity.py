7edc9518c8a0adfd344fc077900e0fb36d9dea92
Russell Loewe <russloewe@gmail.com>
2015-06-22 20:16:30 -0700

    added reader_type and read_file

--- PyGravity/PyGravity.py
+++ PyGravity/PyGravity.py
@@ -3,14 +3,20 @@ import Data_IO
 
 class PyGravity():
 	def __init__(self):
+		self.dimension = 3
 		self.Physics = Physics()
-		self.reader = None
+		self.reader = Data_IO.Reader()
+		self.objects = []
 		
-	def set_reader(self, type):
-		if type == 'CSV':
-			self.reader = Data_IO.CSV_Reader()
+	def set_reader_type(self, r_type):
+		self.reader.dimension = self.dimension
+		self.reader.set_reader_type(r_type)
 		
 	def set_dimension(self, dim):
 		self.Physics.dimension = dim
 		
+	def read_file(self, file_name):
+		self.objects = self.reader.read_file(file_name)
+	
+			
 	
