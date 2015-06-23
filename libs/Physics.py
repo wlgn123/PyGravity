6e2ca9aa598b9c83f38b983ebcc053451d4a3794
Russell Loewe <russloewe@gmail.com>
2015-06-22 18:57:33 -0700

    removed csv reader from physics.py

--- libs/Physics.py
+++ libs/Physics.py
@@ -1,7 +1,7 @@
 from decimal import *
 from Particle import Particle
 from Vector import Vector
-import csv
+
 class Physics(object):
     def __init__(self):
         self.objects = []
@@ -95,18 +95,6 @@ class Physics(object):
         for item in self.objects:
             item.move(self.timestep)
         self.total_steps += 1
-
-    def read_file(self, path):
-        with open(path, 'rb') as csvfile:
-            spamreader = csv.reader(csvfile, delimiter=':', quotechar='|')
-            for row in spamreader:
-                try:
-                    if self.dimension == 3:
-                        self.add_obj(Particle(row[0], Vector(row[1:4]), Vector(row[4:7]), Vector(row[7:]) ) )
-                    elif self.dimension == 2:
-                        self.add_obj(Particle(row[0], Vector(row[1:3]), Vector(row[3:5]), Vector(row[5:]) ) )
-                except ValueError as e:
-                    print e
 
 
 
