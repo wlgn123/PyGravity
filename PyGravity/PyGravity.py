7e3805036ff85fd9553a4dca87990c05161fcb37
Russ <russloewe@gmail.com>
2015-06-23 22:29:44 -0700

    added wrtier from Data_IO to main class

--- PyGravity/PyGravity.py
+++ PyGravity/PyGravity.py
@@ -2,19 +2,22 @@ from Physics import Physics
 import Data_IO
 
 class PyGravity():
-	def __init__(self):
-		self.dimension = 3
-		self.Physics = Physics()
-		self.reader = Data_IO.Reader()
+    def __init__(self):
+        self.dimension = 3
+        self.Physics = Physics()
+        self.reader = Data_IO.Reader()
+        self.writer = Data_IO.Writer()
 
 		
-	def set_dimension(self, dim):
-		self.Physics.dimension = dim
-		self.reader.dimension = dim
+    def set_dimension(self, dim):
+        self.Physics.dimension = dim
+        self.reader.dimension = dim
 		
-	def read_file(self, file_name):
-		self.reader.read_file(file_name)
-		self.Physics.objects = self.reader.objects
-	
-			
+    def read_file(self, file_name):
+        self.reader.read_file(file_name)
+        self.Physics.objects = self.reader.objects
+        
+    def write_file(self, name):
+        self.writer.objects = self.Physics.objects
+        self.writer.write_file(name)
 	
