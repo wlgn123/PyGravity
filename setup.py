4357fb2a7c1f5f7dd2f20c24675e2f3700ed74e1
Russell Loewe <russloewe@gmail.com>
2015-06-30 00:43:10 -0700

    added setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='PyGravity',
+      version='0.1',
+      description='High Precision Gravity Simulator',
+      url='http://github.com/russloewe/PyGravity',
+      author='Russell Loewe',
+      author_email='russloewe@gmail.com',
+      license='MIT',
+      packages=['PyGravity'],
+      zip_safe=False)
