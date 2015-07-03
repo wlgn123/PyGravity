c400c79c9545c0b8c0ebfcf92f457ea9093dc883
Russ <russloewe@gmail.com>
2015-07-02 19:17:12 -0700

    added unit test to setup file

--- setup.py
+++ setup.py
@@ -1,11 +1,30 @@
 from distutils.core import setup
+from distutils.cmd import Command
+from tests import test
+
+
+class TestCommand(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        test.main()
 
 setup(name='PyGravity',
-      version='1.01',
-      description='High Precision Gravity Simulator',
-      url='http://github.com/russloewe/PyGravity',
-      author='Russell Loewe',
-      author_email='russloewe@gmail.com',
-      license='MIT',
-      packages=['PyGravity'],
-      zip_safe=False)
+	version='1.01',
+	description='High Precision Gravity Simulator',
+	url='http://github.com/russloewe/PyGravity',
+	author='Russell Loewe',
+	author_email='russloewe@gmail.com',
+	license='MIT',
+	packages=['PyGravity'],
+	zip_safe=False,
+	cmdclass={'test': TestCommand}
+	)
+
+
