e3c7350bce60afdc19e4af32d28d7042bb543096
Russell Loewe <russloewe@gmail.com>
2015-07-07 21:09:25 -0700

    fixed data writing example

--- examples/data_example.py
+++ examples/data_example.py
@@ -2,6 +2,6 @@ from PyGravity import PyGravity as Pg
 
 base = Pg()
 
-base.read_file('test_data.csv')
+base.read_file('example_data.csv')
 
-base.write_file('test_out.txt')
+base.write_file('example_out.txt')
