49f4b88c6d3f573dcce4a213b37cae86e98139db
Russell Loewe <russloewe@gmail.com>
2015-07-23 21:59:29 -0700

    example changes

--- examples/2D_Physics.py
+++ examples/2D_Physics.py
@@ -9,8 +9,8 @@ base.set_dimension(3)           # not needed for dim 3, but here it is
 
 base.read_file('example_data.csv')
 
-base.set_time_interval(60)
-base.set_precision(200)
+base.set_time_interval(1000)
+base.set_fast(True)
 
 ax = []
 ay = []
@@ -20,19 +20,26 @@ cx = []
 cy = []
 dx = []
 dy = []
-for i in range(90):
-    if i % 1 == 0:
-        print base.particle_list[0].round(2)
-    ax.append(base.particle_list[0].P[0])
-    ay.append(base.particle_list[0].P[1])
-    bx.append(base.particle_list[1].P[0])
-    by.append(base.particle_list[1].P[1])
-    cx.append(base.particle_list[2].P[0])
-    cy.append(base.particle_list[2].P[1])
-    dx.append(base.particle_list[3].P[0])
-    dy.append(base.particle_list[3].P[1])
-
-    base.step_all()
+for i in range(10000):
+    if i % 1000 == 0:
+        print base.particle_list[0].round(2),i
+        print base.particle_list[1].round(2),i
+        print base.particle_list[2].round(2),i
+        print base.particle_list[3].round(2),i
+        ax.append(base.particle_list[0].P[0])
+        ay.append(base.particle_list[0].P[1])
+        bx.append(base.particle_list[1].P[0])
+        by.append(base.particle_list[1].P[1])
+        cx.append(base.particle_list[2].P[0])
+        cy.append(base.particle_list[2].P[1])
+        dx.append(base.particle_list[3].P[0])
+        dy.append(base.particle_list[3].P[1])
+        
+    if PyGravity.Physics.escaping(base.particle_list) != []:
+        print PyGravity.Physics.escaping(base.particle_list)
+        break
+        
+    base.step_all_verlet()
     
 print 'Time: ', (base.currant_time), 'mins'
 plt.scatter(ax, ay, s=10, c='b', alpha=0.5)
