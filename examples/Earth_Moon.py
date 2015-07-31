9cee4a064eb72a4b71e62e205cdd74b69804f344
Russell Loewe <russloewe@gmail.com>
2015-07-30 17:59:12 -0700

    earth moon example

--- examples/Earth_Moon.py
+++ examples/Earth_Moon.py
@@ -7,9 +7,7 @@ import PyGravity
 
 #PyGravity stuff
 base = PyGravity.PyGravity()    # start by making base instance 
-base.set_dimension(3)           # not needed for dim 3, but here it is
-#base.read_file('earth_moon.csv')
-base.read_file('earth_moon.csv')
+base.read_file('earth_moon.xml')
 
 base.set_time_interval(1)
 base.set_fast(True)
@@ -27,40 +25,36 @@ Bx = []
 By = []
 Bz = []
 
-one_day = 60*60*24
-print PyGravity.Physics.Grav_Accel(base.particle_list[0],base.particle_list[1] ).round(4)
-for i in range(one_day * 90 ):
+one_hour = 60*60
+one_day = one_hour* 24
+
+for i in range(one_day * 27 ):
+	# Show what day the simulator is on
 	if i % one_day == 0:
-		#print base.particle_list[0].round(10),i
-		print base.particle_list[1].P.round(2),i/one_day
-	Ax.append(float(base.particle_list[0].P[0]))
-	Ay.append(float(base.particle_list[0].P[1]))
-	Az.append(float(base.particle_list[0].P[2]))
-	Bx.append(float(base.particle_list[1].P[0]))
-	By.append(float(base.particle_list[1].P[1]))
-	Bz.append(float(base.particle_list[1].P[2]))
+		print 'Day: ', i/one_day
+		
+	#plot a point for every hour
+	if i % one_hour == 0:
+		Ax.append(float(base.particle_list[0].P[0]))
+		Ay.append(float(base.particle_list[0].P[1]))
+		Az.append(float(base.particle_list[0].P[2]))
+		Bx.append(float(base.particle_list[1].P[0]))
+		By.append(float(base.particle_list[1].P[1]))
+		Bz.append(float(base.particle_list[1].P[2]))
 
-	
-
-	if i % 500 == 0:
 		if PyGravity.Physics.escaping(base.particle_list) != []:
 			print 'escaping',PyGravity.Physics.escaping(base.particle_list)
 			break
 
 
-	base.step_all_verlet()
-	#base.step_all()
+	base.step_all()
 
 print 'Time: ', (base.currant_time)/(60*60), 'hrs'
 
-#ax.set_xlim(0, 2.5e1)
-#ax.set_ylim(-2.0e1, 2.0e1)
-#ax.set_zlim(-2.0e1, 2.0e1)
 
-ax.plot(Ax, Ay, Az, label='Obj A')
-ax.plot(Bx, By, Bz, label='Obj B')
+ax.plot(Ax, Ay, Az, label='Earth')
+ax.plot(Bx, By, Bz, label='Moon')
 ax.legend()
-#plt.ylim(-2.0e5, 2.0e5)
-#plt.xlim(-2.0e5, 2.0e5)
+
 
 plt.show()
