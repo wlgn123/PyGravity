4c3e42bee5f3d65e45090e8ace96bed820dd9a64
Russell Loewe <russloewe@gmail.com>
2015-07-30 18:54:24 -0700

    doc update

--- examples/2D_Physics.py
+++ examples/2D_Physics.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+import sys
 import PyGravity
 
 base = PyGravity.PyGravity()    # start by making base instance 
@@ -9,7 +9,7 @@ base.set_dimension(3)           # not needed for dim 3, but here it is
 
 base.read_file('example_data.csv')
 
-base.set_time_interval(1000)
+base.set_time_interval(5000000000)
 base.set_fast(True)
 
 ax = []
@@ -20,27 +20,30 @@ cx = []
 cy = []
 dx = []
 dy = []
-for i in range(10000):
-    if i % 1000 == 0:
-        print base.particle_list[0].round(2),i
-        print base.particle_list[1].round(2),i
-        print base.particle_list[2].round(2),i
-        print base.particle_list[3].round(2),i
-        ax.append(base.particle_list[0].P[0])
-        ay.append(base.particle_list[0].P[1])
-        bx.append(base.particle_list[1].P[0])
-        by.append(base.particle_list[1].P[1])
-        cx.append(base.particle_list[2].P[0])
-        cy.append(base.particle_list[2].P[1])
-        dx.append(base.particle_list[3].P[0])
-        dy.append(base.particle_list[3].P[1])
-        
-    if PyGravity.Physics.escaping(base.particle_list) != []:
-        print PyGravity.Physics.escaping(base.particle_list)
-        break
-        
-    base.step_all_verlet()
-    
+for i in range(20000):
+	try:
+		if i % 200 == 0:
+			print i
+			
+			ax.append(base.particle_list[0].P[0])
+			ay.append(base.particle_list[0].P[1])
+			bx.append(base.particle_list[1].P[0])
+			by.append(base.particle_list[1].P[1])
+			cx.append(base.particle_list[2].P[0])
+			cy.append(base.particle_list[2].P[1])
+			dx.append(base.particle_list[3].P[0])
+			dy.append(base.particle_list[3].P[1])
+
+			if PyGravity.Physics.escaping(base.particle_list) != []:
+				print PyGravity.Physics.escaping(base.particle_list)
+				break
+			
+		
+		base.step_all_verlet()
+		
+	except RuntimeWarning:
+		sys.exit(1)
+	
 print 'Time: ', (base.currant_time), 'mins'
 plt.scatter(ax, ay, s=10, c='b', alpha=0.5)
 plt.scatter(bx, by, s=10, c='r', alpha=0.5)
