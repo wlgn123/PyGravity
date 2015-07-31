34e30334b56f2cb353f6e2ee11f5aa0270d63434
Russell Loewe <russloewe@gmail.com>
2015-07-30 19:18:45 -0700

    examples update

--- examples/2D_Physics.py
+++ examples/2D_Physics.py
@@ -1,16 +1,9 @@
-import numpy as np
 import matplotlib.pyplot as plt
-import sys
 import PyGravity
 
-base = PyGravity.PyGravity()    # start by making base instance 
-base.set_dimension(3)           # not needed for dim 3, but here it is
-
-
+base = PyGravity.PyGravity()    # start by making base instance
 base.read_file('example_data.csv')
-
-base.set_time_interval(5000000000)
-base.set_fast(True)
+base.set_time_interval(100)
 
 ax = []
 ay = []
@@ -20,30 +13,30 @@ cx = []
 cy = []
 dx = []
 dy = []
-for i in range(20000):
-	try:
-		if i % 200 == 0:
-			print i
-			
-			ax.append(base.particle_list[0].P[0])
-			ay.append(base.particle_list[0].P[1])
-			bx.append(base.particle_list[1].P[0])
-			by.append(base.particle_list[1].P[1])
-			cx.append(base.particle_list[2].P[0])
-			cy.append(base.particle_list[2].P[1])
-			dx.append(base.particle_list[3].P[0])
-			dy.append(base.particle_list[3].P[1])
 
-			if PyGravity.Physics.escaping(base.particle_list) != []:
-				print PyGravity.Physics.escaping(base.particle_list)
-				break
-			
-		
-		base.step_all_verlet()
-		
-	except RuntimeWarning:
-		sys.exit(1)
-	
+for i in range(200000):
+        if i % 200 == 0:
+                #some print statements to watch while the simulator runs
+                print i
+                print base.particle_list[0].P
+
+                #plot all the postions
+                ax.append(base.particle_list[0].P[0])
+                ay.append(base.particle_list[0].P[1])
+                bx.append(base.particle_list[1].P[0])
+                by.append(base.particle_list[1].P[1])
+                cx.append(base.particle_list[2].P[0])
+                cy.append(base.particle_list[2].P[1])
+                dx.append(base.particle_list[3].P[0])
+                dy.append(base.particle_list[3].P[1])
+
+                #Stop when a particle starts escaping the system
+                if PyGravity.Physics.escaping(base.particle_list) != []:
+                        print PyGravity.Physics.escaping(base.particle_list)
+                        break
+
+        base.step_all()
+
 print 'Time: ', (base.currant_time), 'mins'
 plt.scatter(ax, ay, s=10, c='b', alpha=0.5)
 plt.scatter(bx, by, s=10, c='r', alpha=0.5)
